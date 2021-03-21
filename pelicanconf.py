7b9c396e78305e82edbdea0780d343c75e584a25
Brian Mc Donald <brian@brianmcdonald.me>
2021-03-21 21:31:55 +0100

    fix1

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,8 +18,8 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 # TRANSLATION_FEED_ATOM = None
 # AUTHOR_FEED_ATOM = None
 # AUTHOR_FEED_RSS = None
-PAGE_URL = '{slug}'
-ARTICLE_URL = '{slug}'
+#PAGE_URL = '{slug}'
+#ARTICLE_URL = '{slug}'
 
 # Blogroll
 LINKS = (('Writing', '/#writing'),)
